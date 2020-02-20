bf53f651f9c7fd53a8c3e228661ede38fcfda658
Brian <darkenedmagi@gmail.com>
2020-02-19 23:55:29 -0600

    Human movement

---
+++ The Great Ferret Escape/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform body;
+    float xRotation = 0f;
+    public int rotateUpClamp;
+    public int rotateDownClamp;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation , rotateUpClamp,rotateDownClamp);
+        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
